c8d848cb7a861f230a30253ef48ae7d2accb2a55
Prem Raj <rojarajan@gmail.com>
2022-06-20 17:00:17 -0400

    changes to code

--- kc_2.py
+++ kc_2.py
@@ -5,12 +5,3 @@ expenditures = pd.read_csv(os.path.join(os.path.dirname(__file__), 'assets', 'eE
 
 most_expensive = expenditures.nlargest(10, 'CheckAmt')
 print(most_expensive)
-
-Department = most_expensive["Category"]
-Amount = most_expensive ["CheckAmt"]
-
-plt.bar(Department, Amount, color = 'green')
-plt.xlabel('Name of the Department')
-plt.ylabel('Amount invested')
-plt.title('Top 10 Expenditures 2021')
-plt.show()
